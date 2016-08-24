60a486db46d3d469b4540dca5a387675527e6f0b
Max Drexel <local@localhost>
2016-08-24 19:18:28 -0400

    Initial project layout

---
+++ ImageBird/ImageBird.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ImageBird.Tests {
+	[TestClass]
+	public class UnitTest1 {
+		[TestMethod]
+		public void TestMethod1() {
+		}
+	}
+}
