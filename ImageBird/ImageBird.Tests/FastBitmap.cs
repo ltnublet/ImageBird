c3fe7ce27ec1fc3c25053586ed46be31d34a6618
Max Drexel <lord_onager@yahoo.com>
2016-11-03 21:20:46 -0700

    Nuked the project because I approached it from the wrong direction, and started work on a FastBitmap implementation.

---
+++ ImageBird/ImageBird.Tests/FastBitmap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using SUT = ImageBird;
+
+namespace ImageBird.Tests
+{
+    /// <summary>
+    /// Tests for the ImageBird.FastBitmap class.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Underscores are used to make names human-readable.")]
+    [SuppressMessage("ReSharper", "StyleCop.SA1310", Justification = "Underscores are used to make names human-readable.")]
+    [SuppressMessage("ReSharper", "StyleCop.SA1600", Justification = "Test names should adequately describe the SUT, supplied data, and outcome.")]
+    public class FastBitmap
+    {
+        private const string ResourcePath = @"..\..\Resources\";
+        private const string TestData1_KnownGood = ResourcePath + "TestData1_KnownGood.png";
+        private const string TestData1_GrayScale_KnownGood = ResourcePath + "TestData1_GrayScale_KnownGood.png";
+        private const string TestData1_GrayScale_KnownBad = ResourcePath + "TestData1_GrayScale_KnownBad.png";
+
+        [Fact]
+        public void FastBitmap_NullBitmap_ThrowsArgumentNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SUT.FastBitmap((Bitmap)null));
+        }
+
+        [Fact]
+        public void FastBitmap_InvalidBitmap_ThrowsArgument()
+        {
+            Assert.Throws<ArgumentException>(() => new SUT.FastBitmap(new Bitmap(0, 0)));
+        }
+
+        [Fact]
+        public void FastBitmap_ValidBitmap_ShouldSucceed()
+        {
+            SUT.FastBitmap expected = new SUT.FastBitmap(new Bitmap(Image.FromFile(FastBitmap.TestData1_KnownGood)));
+            expected.Dispose();
+        }
+    }
+}
---
+++ ImageBird/ImageBird/FastBitmap.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageBird
+{
+    /// <summary>
+    /// A Bitmap implementation with improved performance.
+    /// </summary>
+    public class FastBitmap : IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FastBitmap"/> class.
+        /// </summary>
+        /// <param name="bitmap">
+        /// The bitmap to convert to a FastBitmap.
+        /// </param>
+        public unsafe FastBitmap(Bitmap bitmap)
+        {
+            if (bitmap == null)
+            {
+                throw new ArgumentNullException(nameof(bitmap));
+            }
+
+            if (bitmap.Height == 0 || bitmap.Width == 0)
+            {
+                throw new ArgumentException("Supplied bitmap has invalid dimensions.", nameof(bitmap));
+            }
+
+            this.Buffer = (Bitmap)bitmap.Clone();
+        }
+
+        /// <summary>
+        /// Used to perform an action on a pixel.
+        /// </summary>
+        /// <param name="pointer">
+        /// A pointer to the first address of the pixel's data.
+        /// </param>
+        /// <param name="bitsPerPixel">
+        /// The number of bits per pixel.
+        /// </param>
+        protected unsafe delegate void PixelAction(byte* pointer, int bitsPerPixel);
+
+        /// <summary>
+        /// Bitmap to which specified changes will be applied.
+        /// </summary>
+        public Bitmap Buffer { get; protected set; }
+
+        /// <summary>
+        /// Disposes the FastBitmap, freeing it's resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Buffer.Dispose();
+        }
+
+        /// <summary>
+        /// Performs the supplied operation on each pixel in the buffer.
+        /// </summary>
+        /// <param name="operation">
+        /// The operation to perform on each pixel.
+        /// </param>
+        protected unsafe void Operation(PixelAction operation)
+        {
+            BitmapData contents = this.Buffer.LockBits(
+                new Rectangle(0, 0, this.Buffer.Width, this.Buffer.Height),
+                ImageLockMode.ReadOnly,
+                this.Buffer.PixelFormat);
+
+            int bitsPerPixel = Image.GetPixelFormatSize(this.Buffer.PixelFormat);
+
+            byte* scan0 = (byte*)contents.Scan0.ToPointer();
+
+            for (int yPos = 0; yPos < contents.Height; yPos++)
+            {
+                for (int xPos = 0; xPos < contents.Width; xPos++)
+                {
+                    byte* data = scan0 + (yPos * contents.Stride) + ((xPos * bitsPerPixel) / 8);
+
+                    operation(data, bitsPerPixel);
+                }
+            }
+
+            this.Buffer.UnlockBits(contents);
+        }
+    }
+}
